26b8bc8458c78972b692ac03ec8edca3e72cab78
Cindy Mutisya <cindy.mbishmutisya@gmail.com>
2024-08-05 19:55:12 +0300

    restructured a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import UserProfile from './components/UserProfile';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div>
-      <UserProfile />
+      <LoginPage />
     </div>
   );
 }
