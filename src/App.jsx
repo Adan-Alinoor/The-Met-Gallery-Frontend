332c7d66963d86b584c83c0bcf0a3ce99728c767
Cindy Mutisya <cindy.mbishmutisya@gmail.com>
2024-08-07 11:31:28 +0300

    css altered

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
 
