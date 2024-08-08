69ae2bb152f2b0b81537d5bd2274d996eb14bd43
Cindy Mutisya <cindy.mbishmutisya@gmail.com>
2024-08-08 10:43:37 +0300

    errors fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-
-import React, { useState } from "react";
-import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import LoginPage from "./components/LoginPage";
-import HomePage from "./components/HomePage";
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import LoginPage from './components/LoginPage';
+import HomePage from './components/HomePage';
 import UserProfile from './components/UserProfile';
-
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -17,21 +14,25 @@ function App() {
   };
 
   return (
-    <div>
-      {isLoggedIn ? (
-        <HomePage />
-      ) : (
-        <LoginPage onLogin={handleLogin} />
-      )}
-    </div>
-
     <Router>
       <Routes>
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="*" element={<Navigate to="/profile" />} />
+        {/* Public Routes */}
+        <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/home"
+          element={isLoggedIn ? <HomePage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={isLoggedIn ? <UserProfile /> : <Navigate to="/" />}
+        />
+
+        {/* Redirect all other paths to the login page */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
-
   );
 }
 
