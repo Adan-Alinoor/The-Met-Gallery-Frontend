bf6287e164a35284a5e5a7d1a82726de5991cbf6
Brian Kemboi <brnkemboi@gmail.com>
2024-08-16 09:29:18 +0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Footer.css';
 
@@ -8,11 +8,13 @@ const Footer = () => {
       <footer className="footer bg-dark text-light py-4">
         <div className="container">
           <div className="row">
-            <div className="col-md-6">
+            <div className="col-md-12"> {/* Changed to col-md-12 to center text properly */}
               <p className="footer-text">
                 <a href="mailto:metgallery.ke@gmail.com" className="footer-email">Email: metgallery.ke@gmail.com</a>
               </p>
-              <p className="footer-text">Phone: (254) 456-7890</p>
+              <p className="footer-text">
+                <a href="tel:+25471569966" className="footer-phone">Phone: 25471569966</a>
+              </p>
               <p className="footer-text">Address: P.O.BOX 13505-0400</p>
             </div>
           </div>
