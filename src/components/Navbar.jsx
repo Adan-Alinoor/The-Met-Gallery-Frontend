14805ff40653961bc7f8731805eb8a91e7d72023
Cindy Mutisya <cindy.mbishmutisya@gmail.com>
2024-08-16 09:10:34 +0300

    changed name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({ cartItemsCount, isArtist, userGender }) => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">The Met Art Gallery</Link>
+      <Link className="navbar-brand" to="/">The Met Gallery</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
