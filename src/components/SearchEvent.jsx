baa0896d5d31898b8e67e63b1a4a5c902785f4e9
Gloria Wekura <wekuraglorian07@gmail.com>
2024-08-18 16:18:27 +0300

    done adding event search bar

--- src/components/SearchEvent.jsx
+++ src/components/SearchEvent.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './SearchEvent.css';  
 
-function SearchEvent() {
+function SearchEvent({ searchEvent, setSearchEvent }) {
   return (
-    <div>SearchEvent</div>
-  )
+    <div className="search-event-container">
+      <h2>Search Events:</h2>
+      <div className="search-input-container">
+        <input 
+          className="search-input"
+          value={searchEvent}
+          onChange={(e) => setSearchEvent(e.target.value)}
+          type="text"  
+          placeholder="Search for an Event..."
+        />
+      </div>
+    </div>
+  );
 }
 
-export default SearchEvent
+export default SearchEvent;
